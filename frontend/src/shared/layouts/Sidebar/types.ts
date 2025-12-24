e16cd20055ebc0bc001756fb1f081f300266c5d2
llcuong <leelongcuong@gmail.com>
2025-12-24 15:49:01 +0700

    251224-1549

--- frontend/src/shared/layouts/Sidebar/types.ts
+++ frontend/src/shared/layouts/Sidebar/types.ts
@@ -1,9 +1,8 @@
-import type { PageNavigatorProps, ExtraAppConfig } from "@routes/types";
+import type { PageNavigatorProps } from "@routes/types";
 
 export interface SidebarProps extends PageNavigatorProps {
     isSideBarOpen: boolean;
     setSideBarOpen: (value: boolean | ((prev: boolean) => boolean)) => void;
-    extraPrivateApps?: ExtraAppConfig[];
 }
 
 export interface Props {
