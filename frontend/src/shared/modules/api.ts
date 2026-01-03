cf2afe91795dc39cce41bf0cd415d7fd427c4423
llcuong <leelongcuong@gmail.com>
2026-01-03 10:51:12 +0700

    update

--- frontend/src/shared/modules/api.ts
+++ frontend/src/shared/modules/api.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosError, InternalAxiosRequestConfig } from 'axios';
 import { useAuthStore } from './Authentication/useAuthStore';
 
-const API_BASE_URL = 'http://172.18.55.218:10000';
+const API_BASE_URL = 'http://172.18.55.215:10000';
 
 const axiosClient = axios.create({
     baseURL: API_BASE_URL,
